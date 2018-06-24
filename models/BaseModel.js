dee108193d8b4ddb7c355e23027abc8fffb02dac
Jared Carrano <jcarrano@newvisions.org>
2018-06-24 14:41:51 -0400

    Adds afterUpdate & beforeUpdate cbs, tests

--- models/BaseModel.js
+++ models/BaseModel.js
@@ -1,16 +1,59 @@
+const { randomNumBetween } = require('../lib/utils');
+
 class BaseModel {
 
   constructor(pojo) {
     Object.assign(this, pojo);
+    this._beforeUpdateCallbacks = [];
+    this._afterUpdateCallbacks = [];
   }
 
   async update ( patch, options ) {
+
+    await this.runBeforeUpdateCallbacks(patch, options);
+
     // Mock DB update
     const result = await mockDbUpdate(
       { _id: this._id },
       patch
     );
+
+    const x = await this.runAfterUpdateCallbacks(patch, options);
     return result;
+  }
+
+  registerAfterUpdateCallback({ callbackName, callback }) {
+    // Require callbackName to help with debugging
+    this._afterUpdateCallbacks.push({ callbackName, callback });
+  }
+
+  async runAfterUpdateCallbacks(patch, options) {
+    const runCallbacks = this._runCallbacksPartial( this._afterUpdateCallbacks );
+    await runCallbacks( patch, options );
+  }
+  
+  registerBeforeUpdateCallback({ callbackName, callback }) {
+    // Require callbackName to help with debugging
+    this._beforeUpdateCallbacks.push({ callbackName, callback });
+  }
+
+  async runBeforeUpdateCallbacks(patch, options) {
+    const runCallbacks = this._runCallbacksPartial( this._beforeUpdateCallbacks );
+    await runCallbacks( patch, options );
+  }
+
+  _runCallbacksPartial( callbackArray ) {
+    return async ( patch, options ) => {
+      for ( const { callbackName, callback } of callbackArray ){
+        try {
+          await callback(patch, options);
+        } catch ( err ) {
+          // Append the callback name to help with debugging
+          err.callbackName = callbackName;
+          throw err;
+        }
+      }
+    }
   }
 }
 
--- test/models/BaseModel.spec.js
+++ test/models/BaseModel.spec.js
@@ -16,5 +16,191 @@ describe('BaseModel', () => {
       expect(baseModel.favoriteColor).toEqual(pojo.favoriteColor);
     });
   });
+
+  describe('active record callbacks', () => {
+
+    test('callbacks can be registered', () => {
+      class MockChildClass extends BaseModel {
+        constructor(pojo){
+          super(pojo);
+
+          this.registerAfterUpdateCallback({ 
+            callbackName: 'doSomethingAfterCallback', 
+            callback: this.doSomethingAfterCallback
+          });
+
+          this.registerBeforeUpdateCallback({ 
+            callbackName: 'doSomethingAfterCallback', 
+            callback: this.doSomethingAfterCallback
+          });          
+        }
+  
+        doSomethingAfterCallback (patch, options) {
+          return { patch, options };
+        }
+      }      
+      const child = new MockChildClass();
+
+      expect(child._afterUpdateCallbacks.length).toBe(1);
+      expect(child._afterUpdateCallbacks[0].callbackName).toBe('doSomethingAfterCallback');
+
+      expect(child._beforeUpdateCallbacks.length).toBe(1);
+      expect(child._beforeUpdateCallbacks[0].callbackName).toBe('doSomethingAfterCallback');      
+    })
+
+    test('after update callback is called with expected args', async () => {
+      // Create a mock to use as an afterUpdate callback
+      const afterUpdateCallbackSpy = jest.fn();
+
+      class MockChildClass extends BaseModel {
+        constructor(pojo){
+          super(pojo);
+          this.registerAfterUpdateCallback({ 
+            callbackName: 'doSomethingAfterCallback', 
+            callback: this.doSomethingAfterCallback
+          });
+        }
+  
+        doSomethingAfterCallback () {
+          return afterUpdateCallbackSpy.apply(this, arguments);
+        }
+      }      
+      const child = new MockChildClass();
+
+      const testPatch = { name: 'johanna' };
+      const testOptions = { updatingUser: 'jared' };
+      
+      await child.update(testPatch, testOptions);
+
+      expect(afterUpdateCallbackSpy.mock.calls.length).toBe(1);
+      expect(afterUpdateCallbackSpy.mock.calls[0][0]).toBe(testPatch);
+      expect(afterUpdateCallbackSpy.mock.calls[0][1]).toBe(testOptions);
+    }) 
+
+    test('multiple after update callbacks are called with expected args', async () => {
+      // Create a mock to use as an afterUpdate callback
+      const afterUpdateCallbackSpy = jest.fn();
+
+      class MockChildClass extends BaseModel {
+        constructor(pojo){
+          super(pojo);
+          this.registerAfterUpdateCallback({ 
+            callbackName: 'doSomethingAfterCallback', 
+            callback: this.doSomethingAfterCallback
+          });
+
+          this.registerAfterUpdateCallback({ 
+            callbackName: 'doSomethingElseAfterCallback', 
+            callback: this.doSomethingElseAfterCallback
+          });
+          
+          this.registerAfterUpdateCallback({ 
+            callbackName: 'doOneOtherThing', 
+            callback: this.doOneOtherThingAfterCallback
+          });          
+        }
+  
+        doSomethingAfterCallback () {
+          return afterUpdateCallbackSpy.apply(this, arguments);
+        }
+
+        doSomethingElseAfterCallback () {
+          return afterUpdateCallbackSpy.apply(this, arguments);
+        }
+        
+        doOneOtherThingAfterCallback () {
+          return afterUpdateCallbackSpy.apply(this, arguments);
+        }        
+      }      
+      const child = new MockChildClass();
+
+      const testPatch = { name: 'johanna' };
+      const testOptions = { updatingUser: 'jared' };
+      
+      await child.update(testPatch, testOptions);
+
+      expect(afterUpdateCallbackSpy.mock.calls.length).toBe(3);
+    }) 
+    
+    test('multiple after update callbacks are called in series', async () => {
+      const resolveOrder = [];
+      function getTestPromise (i, time) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolveOrder.push(i);
+            debugger;
+            resolve(i);
+          }, time);
+        });
+      }
+
+      class MockChildClass extends BaseModel {
+        constructor(pojo){
+          super(pojo);
+          this.registerAfterUpdateCallback({ 
+            callbackName: 'doSomethingAfterCallback', 
+            callback: this.doSomethingAfterCallback
+          });
+
+          this.registerAfterUpdateCallback({ 
+            callbackName: 'doSomethingElseAfterCallback', 
+            callback: this.doSomethingElseAfterCallback
+          });
+          
+          this.registerAfterUpdateCallback({ 
+            callbackName: 'doOneOtherThingAfterCallback', 
+            callback: this.doOneOtherThingAfterCallback
+          });          
+        }
+  
+        async doSomethingAfterCallback () {
+          await getTestPromise(1, 400);
+        }
+
+        async doSomethingElseAfterCallback() {
+          await getTestPromise(2, 200);
+        }
+
+        async doOneOtherThingAfterCallback() {
+          await getTestPromise(3, 100);
+        }
+      }
+
+      const child = new MockChildClass({ name: 'jared' });
+
+      await child.update();
+
+      expect(resolveOrder).toEqual([1, 2, 3]);
+    })
+    
+    test('before update callback is called with expected args', async () => {
+      // Create a mock to use as an afterUpdate callback
+      const beforeUpdateCb = jest.fn();
+
+      class MockChildClass extends BaseModel {
+        constructor(pojo){
+          super(pojo);
+          this.registerBeforeUpdateCallback({ 
+            callbackName: 'doSomethingBeforeCallback', 
+            callback: this.doSomethingBeforeCallback
+          });
+        }
+  
+        doSomethingBeforeCallback (options) {
+          return beforeUpdateCb.apply(this, arguments);
+        }
+      }      
+      const child = new MockChildClass();
+
+      const testPatch = { name: 'johanna' };
+      const testOptions = { updatingUser: 'jared' };
+      
+      await child.update(testPatch, testOptions);
+
+      expect(beforeUpdateCb.mock.calls.length).toBe(1);
+      expect(beforeUpdateCb.mock.calls[0][0]).toBe(testPatch);
+      expect(beforeUpdateCb.mock.calls[0][1]).toBe(testOptions);
+    })     
+  });
 });
 
